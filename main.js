a65ffba7a272e1fb662f3c97a60eb526398831e3
kojimano <nk654@cornell.edu>
2023-02-10 12:10:56 -0500

    Making the script intentionally buggy.

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const {ESLint} = require('eslint');
-
+const {ESLint} = require('eslint');
 function msg(){  
  alert("Hello Javatpoint");  
- console.log("Hello World!");
- var somthing = 1 + 3 //intentionally buggy
 }  
 
